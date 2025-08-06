732abf92848dc860b4d987f109805d39a2489e0b
Luo-Annie <luo.701@osu.edu>
2025-08-06 15:29:28 -0400

    hover animation

--- src/components/projectSections/ClickableImage.tsx
+++ src/components/projectSections/ClickableImage.tsx
@@ -12,16 +12,17 @@ const ClickableImage = ({
     return (
         <section>
             <div className="max-w-5xl mx-10 md:mx-auto flex flex-col items-left py-2">
-                <a href={`/case-studies/${header}`}>
+                <a href={`/case-studies/${header}`} className="relative group">
                 <img
                     src={image}
                     alt={header}
-                    className="w-full"
-                />      
-                </a>          
+                    className="w-full transition duration-300 ease-in-out"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-10 transition duration-300 rounded" />
+                </a>
             </div>
         </section>
-    )
+    );
 }
 
 export default ClickableImage;
